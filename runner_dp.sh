efe53ac0b09d822b7f2f93dc69378c02752d02d4
smtnkc <smtnkc@gmail.com>
2020-10-10 12:29:51 +0300

    reformetted runner scripts

--- runner_dp.sh
+++ runner_dp.sh
@@ -3,7 +3,7 @@ for model in "distilbert" "bert"; do
     for data_dir in "data/dp"; do
         for crop in 0.25 1.0; do
             for labels in "mean_class" "median_class" "sum_class"; do
-                python3 main.py --device='cuda' --model=$model --data_dir=$data_dir --crop=$crop --labels=$labels --epochs=3 --save_models=False --seed=1234
+                python3 -W ignore main.py --device='cuda' --model=$model --data_dir=$data_dir --crop=$crop --labels=$labels --epochs=3 --save_models=False --seed=123
             done
         done
     done
